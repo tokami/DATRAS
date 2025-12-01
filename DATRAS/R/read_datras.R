faf604d9a699f5d63253e0f597f6d733e9b7fcb7
tokami <t.k.mildenberger@gmail.com>
2025-12-01 12:01:56 +0100

    add survey to haul.id

--- DATRAS/R/read_datras.R
+++ DATRAS/R/read_datras.R
@@ -481,7 +481,7 @@ addExtraVariables <- function(IBTS){
   }
   d3 <- mytransform(d3)
   if(!is.null(d1)) d1 <- mytransform(d1)
-  haul.id <- quote( factor(paste(Year,Quarter,Country,Ship,Gear,StNo,HaulNo,sep=":"))  )
+  haul.id <- quote( factor(paste(Survey,Year,Quarter,Country,Ship,Gear,StNo,HaulNo,sep=":"))  )
   if(!is.null(d1)) d1$haul.id <- eval(haul.id,d1)
   d2$haul.id <- eval(haul.id,d2)
   d3$haul.id <- eval(haul.id,d3)
