51ec827e40897b882f0f02392b3facc0b429c7f5
tokami <t.k.mildenberger@gmail.com>
2026-02-09 16:34:28 +0100

    clarify duplicated printing

--- DATRAS/R/read_datras.R
+++ DATRAS/R/read_datras.R
@@ -13,20 +13,24 @@
 ##'     matched when filled in, if FALSE a random match will be assigned.
 ##' @param download.ca if FALSE, biological samples in DATRAS (CA data set) are
 ##'     not downloaded
+##' @param verbose Print information? Default: TRUE
 ##' @return DATRASraw object.
 ##' @importFrom icesDatras getDATRAS
 ##' @export
 getDatrasExchange <- function(survey, years, quarters, strict = TRUE,
-                              download.ca = TRUE) {
+                              download.ca = TRUE, verbose = TRUE) {
     ## download data
-    if (download.ca) {
+  if (download.ca) {
+  if (verbose) message("Downloading CA")
         ca <- getDATRAS("CA", survey = survey, years = years, quarters = quarters)
         if (identical(ca, FALSE)) {
             stop()
         }
     } else ca <- NULL
 
-    hh <- getDATRAS("HH", survey = survey, years = years, quarters = quarters)
+  if (verbose) message("Downloading HH")
+  hh <- getDATRAS("HH", survey = survey, years = years, quarters = quarters)
+  if (verbose) message("Downloading HL")
     hl <- getDATRAS("HL", survey = survey, years = years, quarters = quarters)
 
     ## form data into a list- must come in specific order
